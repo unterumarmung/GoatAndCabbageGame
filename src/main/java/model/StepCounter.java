4182b8e70a44d9fdd08db92561281087fd550c2e
unterumarmung <umarmungenfuerimmer@gmail.com>
2020-04-22 20:32:19 +0400

    Реализовал счётчик шагов

---
+++ src/main/java/model/StepCounter.java
@@ -0,0 +1,25 @@
+package model;
+
+import model.exceptions.NoEnoughStepsException;
+
+public class StepCounter {
+    private int _steps;
+
+    public int steps() {
+        return _steps;
+    }
+
+    public StepCounter(int steps) {
+        _steps = steps;
+    }
+
+    public void decrease(int stepsToDecrease) {
+        assertHasSteps(stepsToDecrease);
+        _steps -= stepsToDecrease;
+    }
+
+    private void assertHasSteps(int stepsToDecrease) {
+        if (_steps - stepsToDecrease >= 0)
+            throw new NoEnoughStepsException();
+    }
+}

97a4f9a41e607e77ca7f8265af6f299e953f7ee1
unterumarmung <umarmungenfuerimmer@gmail.com>
2020-04-22 20:34:33 +0400

    Реализовал билдера поля простого

---
+++ src/main/java/model/FieldBuilder.java
@@ -0,0 +1,26 @@
+package model;
+
+import events.MessageSender;
+import org.jetbrains.annotations.NotNull;
+
+public abstract class FieldBuilder {
+    private final @NotNull MessageSender _messageSender;
+    protected GameField _gameField;
+
+    protected FieldBuilder(@NotNull MessageSender messageSender) {
+        _messageSender = messageSender;
+    }
+
+    public GameField build() {
+        _gameField = new GameField(fieldWidth(), fieldHeight(),exitPoint(), _messageSender);
+        addGoat();
+        addWalls();
+        return _gameField;
+    }
+
+    protected abstract int fieldHeight();
+    protected abstract int fieldWidth();
+    protected abstract Point exitPoint();
+    protected abstract void addGoat();
+    protected abstract void addWalls();
+}
---
+++ src/main/java/model/SimpleFieldBuilder.java
@@ -0,0 +1,41 @@
+package model;
+
+import events.MessageSender;
+import org.jetbrains.annotations.NotNull;
+
+public class SimpleFieldBuilder extends FieldBuilder {
+    private static final int FIELD_HEIGHT = 5;
+    private static final int FIELD_WIDTH = 5;
+    private static final int DEFAULT_STEPS_COUNT = 10;
+
+    public SimpleFieldBuilder(@NotNull MessageSender messageSender) {
+        super(messageSender);
+    }
+
+    @Override
+    protected int fieldHeight() {
+        return FIELD_HEIGHT;
+    }
+
+    @Override
+    protected int fieldWidth() {
+        return FIELD_WIDTH;
+    }
+
+    @Override
+    protected Point exitPoint() {
+        return new Point(3, 3);
+    }
+
+    @Override
+    protected void addGoat() {
+        var goat = new Goat(new StepCounter(DEFAULT_STEPS_COUNT));
+        goat.setPosition(_gameField.cell(new Point(0,0)));
+    }
+
+    @Override
+    protected void addWalls() {
+        new Wall(_gameField.cell(new Point(2, 3)));
+        new Wall(_gameField.cell(new Point(3, 2)));
+    }
+}

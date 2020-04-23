ff618741d3aff1f6c2a7a90e586e27b8883afe63
Daniil Dudkin <umarmungenfuerimmer@gmail.com>
2020-04-24 00:43:23 +0400

    Добавил equals и hashCode

--- src/main/java/model/events/CellMessage.java
+++ src/main/java/model/events/CellMessage.java
@@ -5,6 +5,8 @@ import model.Cell;
 import model.GameObject;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
+
 public class CellMessage extends MessageData {
     public CellMessage(@NotNull Type type, @NotNull Cell cell, @NotNull GameObject object) {
         this.type = type;
@@ -20,4 +22,19 @@ public class CellMessage extends MessageData {
     public final @NotNull Type type;
     public final @NotNull Cell cell;
     public final @NotNull GameObject object;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CellMessage that = (CellMessage) o;
+        return type == that.type &&
+                cell.equals(that.cell) &&
+                object.equals(that.object);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type, cell, object);
+    }
 }

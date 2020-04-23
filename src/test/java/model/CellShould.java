7a9f15d7959c467da167c441f7689a17360b69e0
Daniil Dudkin <umarmungenfuerimmer@gmail.com>
2020-04-24 00:44:26 +0400

    Добавил тесты на Cell

---
+++ src/test/java/model/CellShould.java
@@ -0,0 +1,122 @@
+package model;
+
+import events.MessageSender;
+import model.events.CellMessage;
+import model.exceptions.CellAlreadyHasNeighborForDirectionException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import utils.Direction;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CellShould {
+    private MessageSender _messageSender;
+    private Cell _neighborCell;
+    private Direction _direction;
+    private GameObject _gameObject;
+
+    @BeforeEach
+    private void beforeEach() {
+        _messageSender = Mockito.mock(MessageSender.class);
+        _neighborCell = Mockito.mock(Cell.class);
+        _direction = Direction.NORTH;
+        _gameObject = Mockito.mock(GameObject.class);
+    }
+
+    @Test
+    void addNeighbor_whenNoNeighborInDirection() {
+        // Arrange
+        var cell = new Cell(_messageSender);
+
+        // Act
+        cell.setNeighbor(_neighborCell, _direction);
+
+        // Assert
+        assertEquals(_direction, cell.isNeighbor(_neighborCell));
+        assertSame(_neighborCell, cell.neighborCell(_direction));
+    }
+
+    @Test
+    void throw_whenAddNewNeighborInUsedDirection() {
+        // Arrange
+        var cell = new Cell(_messageSender);
+        cell.setNeighbor(_neighborCell, _direction);
+        var newCell = Mockito.mock(Cell.class);
+
+        // Act & Assert
+        assertThrows(CellAlreadyHasNeighborForDirectionException.class, () -> cell.setNeighbor(newCell, _direction));
+    }
+
+    @Test
+    void notThrow_whenAddSameNeighborInUsedDirection() {
+        // Arrange
+        var cell = new Cell(_messageSender);
+        cell.setNeighbor(_neighborCell, _direction);
+
+        // Act & Assert
+        assertDoesNotThrow(() -> cell.setNeighbor(_neighborCell, _direction));
+    }
+
+    @Test
+    void setItselfAsNeighbor_whenNewNeighborAdded() {
+        // Arrange
+        var cell = new Cell(_messageSender);
+
+        // Act
+        cell.setNeighbor(_neighborCell, _direction);
+
+        // Assert
+        Mockito.verify(_neighborCell).setNeighbor(cell, _direction.opposite());
+    }
+
+    @Test
+    void addObject() {
+        // Arrange
+        var cell = new Cell(_messageSender);
+
+        // Act
+        cell.addObject(_gameObject);
+
+        // Assert
+        assertTrue(cell.objects().contains(_gameObject));
+    }
+
+    @Test
+    void removeObject() {
+        // Arrange
+        var cell = new Cell(_messageSender);
+        cell.addObject(_gameObject);
+
+        // Act
+        cell.removeObject(_gameObject);
+
+        // Assert
+        assertFalse(cell.objects().contains(_gameObject));
+    }
+
+    @Test
+    void sendMessage_whenObjectAdded() {
+        // Arrange
+        var cell = new Cell(_messageSender);
+        var messageToBeSent = new CellMessage(CellMessage.Type.OBJECT_ENTERED, cell, _gameObject);
+        // Act
+        cell.addObject(_gameObject);
+
+        // Assert
+        Mockito.verify(_messageSender).emitMessage(cell, messageToBeSent);
+    }
+
+    @Test
+    void sendMessage_whenObjectRemoved() {
+        // Arrange
+        var cell = new Cell(_messageSender);
+        var messageToBeSent = new CellMessage(CellMessage.Type.OBJECT_LEAVED, cell, _gameObject);
+        cell.addObject(_gameObject);
+        // Act
+        cell.removeObject(_gameObject);
+
+        // Assert
+        Mockito.verify(_messageSender).emitMessage(cell, messageToBeSent);
+    }
+}

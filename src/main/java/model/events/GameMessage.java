b2e2282374b6022516a94c0f9496ca0d7113c17c
Daniil Dudkin <umarmungenfuerimmer@gmail.com>
2020-04-24 00:43:30 +0400

    Добавил equals и hashCode

--- src/main/java/model/events/GameMessage.java
+++ src/main/java/model/events/GameMessage.java
@@ -4,10 +4,25 @@ import events.MessageData;
 import model.GameState;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
+
 public class GameMessage extends MessageData {
     public final @NotNull GameState gameState;
 
     public GameMessage(@NotNull GameState gameState) {
         this.gameState = gameState;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GameMessage that = (GameMessage) o;
+        return gameState == that.gameState;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(gameState);
     }
 }

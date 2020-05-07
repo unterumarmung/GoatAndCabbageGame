a9887aca4f453ab9d3e623a3426106437bd26fec
Daniil Dudkin <umarmungenfuerimmer@gmail.com>
2020-05-07 17:24:35 +0400

    Реализовал сущность получения изображений для игры

---
+++ src/main/java/view/providers/FileImageProvider.java
@@ -0,0 +1,32 @@
+package view.providers;
+
+import org.jetbrains.annotations.NotNull;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class FileImageProvider implements ImageProvider {
+    private static final String FILENAME_PREFIX = "resources/";
+    private final @NotNull String filename;
+    private BufferedImage image;
+
+    public FileImageProvider(@NotNull String filename) {
+        this.filename = filename;
+    }
+
+    @Override
+    public BufferedImage image() {
+        if (image != null)
+            return image;
+
+        try {
+            image = ImageIO.read(new File(FILENAME_PREFIX + filename));
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+
+        return image;
+    }
+}

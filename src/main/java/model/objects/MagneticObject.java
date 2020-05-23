9e03690c2b965fe228524bd16832576efef27167
Daniil <umarmungenfuerimmer@gmail.com>
2020-05-24 01:12:03 +0400

    Сделал магнитный объект зацепляемым

--- src/main/java/model/objects/MagneticObject.java
+++ src/main/java/model/objects/MagneticObject.java
@@ -2,6 +2,6 @@ package model.objects;
 
 import utils.collections.ReadOnlyList;
 
-public interface MagneticObject extends GameObject {
+public interface MagneticObject extends HookableObject {
     ReadOnlyList<MagneticPoleWithDirection> magneticPoles();
 }

5517e99eed46965dd11490254513d572de2b0c5e
Daniil <umarmungenfuerimmer@gmail.com>
2020-05-23 22:51:25 +0400

    Реализовал простую коробку

---
+++ src/main/java/model/objects/SimpleBox.java
@@ -0,0 +1,39 @@
+package model.objects;
+
+import model.Cell;
+import org.jetbrains.annotations.NotNull;
+import utils.Direction;
+
+public class SimpleBox implements Box {
+    private Cell cell;
+
+    public SimpleBox(@NotNull Cell initialCell) {
+        this.cell = initialCell;
+    }
+
+    public void move(@NotNull Direction direction) {
+        if (canMoveTo(direction)) {
+            setCell(cell.neighborCell(direction));
+        }
+    }
+
+    void setCell(Cell cell) {
+        if (this.cell != null)
+            this.cell.removeObject(this);
+        if (cell != null)
+            cell.addObject(this);
+        this.cell = cell;
+    }
+
+    @Override
+    public boolean canMoveTo(@NotNull Direction direction) {
+        var neighbor = cell.neighborCell(direction);
+        return neighbor != null
+                && neighbor.objects().stream().noneMatch(GameObject::isSolid);
+    }
+
+    @Override
+    public Cell cell() {
+        return cell;
+    }
+}

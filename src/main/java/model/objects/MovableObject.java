eeb3c362269259135ea55396a008575fa1858520
Daniil <umarmungenfuerimmer@gmail.com>
2020-05-23 22:51:11 +0400

    Реализовал абстракцию объекта, который может двигаться

---
+++ src/main/java/model/objects/MovableObject.java
@@ -0,0 +1,9 @@
+package model.objects;
+
+import org.jetbrains.annotations.NotNull;
+import utils.Direction;
+
+public interface MovableObject extends GameObject {
+    void move(@NotNull Direction direction);
+    boolean canMoveTo(@NotNull Direction direction);
+}

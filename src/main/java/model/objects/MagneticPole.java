96ac9328150ee83dd4be9cd68ee21f4643649e7b
Daniil <umarmungenfuerimmer@gmail.com>
2020-05-26 21:54:31 +0400

    Добавил метод MagneticPole.opposite

--- src/main/java/model/objects/MagneticPole.java
+++ src/main/java/model/objects/MagneticPole.java
@@ -1,8 +1,21 @@
 package model.objects;
 
+import utils.Direction;
+
 public enum MagneticPole {
     NORTH,
     SOUTH;
+
+    static {
+        NORTH.opposite = SOUTH;
+        SOUTH.opposite = NORTH;
+    }
+
+    private MagneticPole opposite;
+
+    public MagneticPole opposite() {
+        return opposite;
+    }
 
     @Override
     public String toString() {

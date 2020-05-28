3a0840b5abe0795e18369e5633c9ada74bb6ebb0
Daniil <umarmungenfuerimmer@gmail.com>
2020-05-28 18:45:35 +0400

    Вынес обработки коллекций в приватные методы

--- src/main/java/model/objects/MovableHookable.java
+++ src/main/java/model/objects/MovableHookable.java
@@ -4,8 +4,10 @@ import model.Cell;
 import org.jetbrains.annotations.NotNull;
 import utils.Direction;
 import utils.Pair;
+import utils.collections.ReadOnlyList;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -23,7 +25,7 @@ public abstract class MovableHookable implements MovableObject, HookableObject {
 
     protected boolean move(@NotNull Direction direction, Set<MovableHookable> alreadyMoved) {
         if (canMoveTo(direction) && !alreadyMoved.contains(this)) {
-            var hookedObjects = hookedObjects().stream().map(pair -> (MovableHookable) pair.first).collect(Collectors.toList());
+            var hookedObjects = castHookedToMovableHookable();
 
             var neighborCell = cell.neighborCell(direction);
             setCell(neighborCell);
@@ -36,6 +38,11 @@ public abstract class MovableHookable implements MovableObject, HookableObject {
             return true;
         }
         return false;
+    }
+
+    @NotNull
+    private List<MovableHookable> castHookedToMovableHookable() {
+        return hookedObjects().stream().map(pair -> (MovableHookable) pair.first).collect(Collectors.toList());
     }
 
     void setCell(Cell cell) {
@@ -52,28 +59,43 @@ public abstract class MovableHookable implements MovableObject, HookableObject {
         if (hookedObjects.isEmpty() && canMoveToIndependent(direction))
             return true;
 
-        var allHookedAreMovable = hookedObjects.stream().allMatch(hookedObject -> hookedObject.first instanceof MovableObject);
-        if (!allHookedAreMovable)
+        if (!allHookedAreMovable(hookedObjects))
             return false;
 
-        var movableHookedObjects = hookedObjects.stream().map(Pair::<MovableObject>castFirst).collect(Collectors.toList());
+        var movableHookedObjects = castHookedToMovable(hookedObjects);
 
-        var allHookedCanMoveExceptOppositeObjects =
-                movableHookedObjects.stream()
-                        .filter(hookedObject -> hookedObject.second != direction.opposite())
-                        .allMatch(hookedObject -> hookedObject.first.canMoveTo(direction));
-        var allOppositeObjectsCanReplaceThis =
-                movableHookedObjects.stream()
-                        .filter(hookedObject -> hookedObject.second == direction.opposite())
-                        .allMatch(hookedObject -> hookedObject.first.canReplace(this, direction));
+        var canMove = allHookedCanMoveExceptOppositeObjects(movableHookedObjects, direction)
+                && allOppositeObjectsCanReplaceThis(movableHookedObjects, direction);
 
-        var canMove = allHookedCanMoveExceptOppositeObjects && allOppositeObjectsCanReplaceThis;
-
-        var isAnyOfHookedInDirection = hookedObjects.stream().anyMatch(hookedObject -> hookedObject.second == direction);
-        if (!isAnyOfHookedInDirection)
+        if (!isAnyOfHookedInDirection(hookedObjects, direction))
             canMove &= canMoveToIndependent(direction);
 
         return canMove;
+    }
+
+    private boolean isAnyOfHookedInDirection(@NotNull ReadOnlyList<Pair<HookableObject, Direction>> hookedObjects, @NotNull Direction direction) {
+        return hookedObjects.stream().anyMatch(hookedObject -> hookedObject.second == direction);
+    }
+
+    @NotNull
+    private List<Pair<MovableObject, Direction>> castHookedToMovable(@NotNull ReadOnlyList<Pair<HookableObject, Direction>> hookedObjects) {
+        return hookedObjects.stream().map(Pair::<MovableObject>castFirst).collect(Collectors.toList());
+    }
+
+    private boolean allHookedAreMovable(@NotNull ReadOnlyList<Pair<HookableObject, Direction>> hookedObjects) {
+        return hookedObjects.stream().allMatch(hookedObject -> hookedObject.first instanceof MovableObject);
+    }
+
+    private boolean allHookedCanMoveExceptOppositeObjects(@NotNull List<Pair<MovableObject, Direction>> movableHookedObjects, @NotNull Direction direction) {
+        return movableHookedObjects.stream()
+                .filter(hookedObject -> hookedObject.second != direction.opposite())
+                .allMatch(hookedObject -> hookedObject.first.canMoveTo(direction));
+    }
+
+    private boolean allOppositeObjectsCanReplaceThis(@NotNull List<Pair<MovableObject, Direction>> movableHookedObjects, @NotNull Direction direction) {
+        return movableHookedObjects.stream()
+                .filter(hookedObject -> hookedObject.second == direction.opposite())
+                .allMatch(hookedObject -> hookedObject.first.canReplace(this, direction));
     }
 
     protected abstract boolean canMoveToIndependent(@NotNull Direction direction);

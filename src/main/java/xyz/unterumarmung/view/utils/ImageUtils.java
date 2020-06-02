72a7515e631b85f21356dd625312817a7d743712
Daniil <umarmungenfuerimmer@gmail.com>
2020-06-02 23:25:59 +0400

    Добавил метод ImageUtils.rotateImageByDegrees

--- src/main/java/xyz/unterumarmung/view/utils/ImageUtils.java
+++ src/main/java/xyz/unterumarmung/view/utils/ImageUtils.java
@@ -1,7 +1,12 @@
 package xyz.unterumarmung.view.utils;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
+
+import static java.lang.Math.*;
 
 public class ImageUtils {
     public static BufferedImage resizeImage(BufferedImage image, Dimension dimension) {
@@ -13,5 +18,33 @@ public class ImageUtils {
         resizedImageGraphics.dispose();
 
         return resizedImage;
+    }
+
+    @SuppressWarnings("IntegerDivisionInFloatingPointContext")
+    public static @NotNull BufferedImage rotateImageByDegrees(@NotNull BufferedImage image, int angle) {
+        final double radians = toRadians(angle);
+        final double sin = abs(sin(radians));
+        final double cos = abs(cos(radians));
+        final int imageWidth = image.getWidth();
+        final int imageHeight = image.getHeight();
+        final int newImageWidth = (int) floor(imageWidth * cos + imageHeight * sin);
+        final int newImageHeight = (int) floor(imageHeight * cos + imageWidth * sin);
+
+        final var rotatedImage = new BufferedImage(newImageWidth, newImageHeight, BufferedImage.TYPE_INT_ARGB);
+        final var rotatedImageGraphics = rotatedImage.createGraphics();
+        final var affineTransform = new AffineTransform();
+        affineTransform.translate((newImageWidth - imageWidth) / 2, (newImageHeight - imageHeight) / 2);
+
+        int x = imageWidth / 2;
+        int y = imageHeight / 2;
+
+        affineTransform.rotate(radians, x, y);
+        rotatedImageGraphics.setTransform(affineTransform);
+        rotatedImageGraphics.drawImage(image, 0, 0, null);
+        rotatedImageGraphics.setColor(Color.RED);
+        rotatedImageGraphics.drawRect(0, 0, newImageWidth - 1, newImageHeight - 1);
+        rotatedImageGraphics.dispose();
+
+        return rotatedImage;
     }
 }
